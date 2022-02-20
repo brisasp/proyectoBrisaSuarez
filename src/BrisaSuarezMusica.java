47d1c3c713b4d3cc563927f6af76afe49502f474
brisa <brisa@air-de-brisa.home>
2022-02-20 20:48:22 +0100

    1000 veces nombre Hobby

--- src/BrisaSuarezMusica.java
+++ src/BrisaSuarezMusica.java
@@ -6,12 +6,26 @@ public class BrisaSuarezMusica {
 	private String genero;
 	private String nombreHobby;
 	
+	
+
+private static void main(String[] args) {
+	System.out.println("La zona ");                                                            
+    int numero=1;
+    
+    while(numero<=1000) { 
+        System.out.println("La zona"); 
+        numero++;
+    }
+	
+}
+	
 public BrisaSuarezMusica(String titulocancion , String genero, String nombreHobby) {
 		super();
 		this.setTitulocancion(titulocancion);
 		this.setGenero(genero);
 		this.setNombreHobby(nombreHobby);
 }
+
 
 public String getTitulocancion() {
 	return titulocancion;
