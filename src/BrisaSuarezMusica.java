42ca7fd2e7b338fb09a755a1e39b33fdb9ca7366
brisa <brisa@air-de-brisa.home>
2022-02-20 19:45:18 +0100

    Segundo commit

--- src/BrisaSuarezMusica.java
+++ src/BrisaSuarezMusica.java
@@ -1,4 +1,16 @@
 
 public class BrisaSuarezMusica {
 
+
+	public String titulocancion;
+	public String genero;
+	public String nombreHobby;
+	
+public BrisaSuarezMusica(String titulocancion , String genero, String nombreHobby) {
+		super();
+		this.titulocancion = titulocancion;
+		this.genero = genero;
+		this.nombreHobby = nombreHobby;
+}
+	
 }
